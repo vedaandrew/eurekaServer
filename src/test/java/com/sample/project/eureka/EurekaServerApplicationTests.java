b15a259b9ec10a9a0762ee5cf2b4d965e73df960
VAArockiam <vaarockiam@Vedas-MacBook-Pro.local>
2024-05-24 17:02:35 +0100

    eureka server spring project

---
+++ src/test/java/com/sample/project/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.project.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
